e466b3d3e1f9beb1612d060b6534722bdde40ab7
d-vo <dominik@van-opdenbosch.de>
2021-02-13 18:10:30 +0100

    Initial commit

---
+++ backup/scripts/params.py
@@ -0,0 +1,14 @@
+PARAMS = {
+    'DATA_DIR': '',
+    'BACKUP_DIR': '',
+    'MYSQL_HOST': '',
+    'MYSQL_USER': '',
+    'MYSQL_PASSWORD': '',
+    'MYSQL_DATABASE': '',
+    'SEND_MAIL': '',
+    'SMTP_TO': '',
+    'SMTP_FROM': '',
+    'SMTP_PWD': '',
+    'SMTP_HOST': '',
+    'SMTP_PORT': '',
+}
